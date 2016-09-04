748dfed1f821fc2ab9697b1f88fec763363ee964
Denis <den.kitaev364@yandex.ru>
2016-09-05 00:52:16 +0300

    Add skeleton

---
+++ game/battlefield.py
@@ -0,0 +1,12 @@
+
+class Ship:
+    def __init__(self, size):
+        self.size = size
+
+
+class BattleField:
+    def __init__(self, ui):
+        self.ui = ui
+
+    def initialize(self):
+        pass
